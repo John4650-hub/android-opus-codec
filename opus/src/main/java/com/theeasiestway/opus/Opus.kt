0e53e4f379e328e9c3983204a220b9672e1d09b2
John Delvin <84484398+John4650-hub@users.noreply.github.com>
2026-06-07 14:38:35 +0300

    Update writeChunk function signature
    Removed frameSize parameter from writeChunk function.

--- opus/src/main/java/com/theeasiestway/opus/Opus.kt
+++ opus/src/main/java/com/theeasiestway/opus/Opus.kt
@@ -78,7 +78,7 @@ class Opus {
     external fun convert(shorts: ShortArray): ByteArray?
     // opusenc save
     external fun oggEncoderInit(path: String, sampleRate: Int, channels: Int, family: Int): Int
-    external fun writeChunk(pcmData: ShortArray, channels: Int,frameSize:Int,denoising:Boolean): Int
+    external fun writeChunk(pcmData: ShortArray, channels: Int,denoising:Boolean): Int
     external fun closeOggEncoder()
     //opusfile
     external fun openFile(fileName:String):Int
